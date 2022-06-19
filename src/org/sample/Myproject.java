5e6371c8d9aba02d8d9a23314a265bc3b2f926f4
premkannan001 <premkannan001@gmail.com>
2022-06-19 14:52:18 +0530

    some projects to be done

---
+++ src/org/sample/Myproject.java
@@ -0,0 +1,15 @@
+package org.sample;
+
+public class Myproject {
+	public static void main(String[] args) {
+	
+	}
+    private void employee() {
+        System.out.println("prem");
+        System.out.println("kumar");
+        System.out.println("green");
+        System.out.println("red");
+        System.out.println("yellow");
+        System.out.println("block");
+	}
+}
